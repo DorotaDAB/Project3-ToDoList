12efbebfd929f9d74f30618295ed43327a43090b
DorotaDAB <dorota.dabrowska.kowalska@gmail.com>
2019-07-17 21:59:07 +0200

    Functionality ready, no styling

--- script.js
+++ script.js
@@ -2,8 +2,14 @@ let $list;
 let $addTodo;
 let $myInput;
 let id = 1;
+let $cancelPopup;
+let $closeX;
+let $popUpInput;
+let $okPopUp;
+let editedElement;
+let popup;
 
-const initialList = ['ToDo1', 'ToDo2', 'ToDo3'];
+const initialList = ['Task1', 'Task2', 'Task3'];
 
 function main() {
     prepareDOMElements();
@@ -15,26 +21,44 @@ function prepareDOMElements() {
     $list = document.getElementById('list');
     $addTodo = document.getElementById('addTodo');
     $myInput = document.getElementById('myInput');
+    $cancelPopup = document.getElementById('cancelPopup');
+    $closeX = document.getElementById('closeX');
+    $popUpInput = document.getElementById('popUpInput');
+    $okPopUp = document.getElementById('okPopUp');
+    popup = document.getElementById('popUp');
 }
 
 function prepareDOMEvents() {
-    $addTodo.addEventListener('click', addButtonClickHandler); //na klik doda nowy elememt do listy
-    $list.addEventListener('click', listClickManager); //sprawdź co kliknęliśmy
+    $addTodo.addEventListener('click', addButtonClickHandler);
+    $myInput.addEventListener('keypress', enterPushHandler); //dodaje z Input na enter
+    $list.addEventListener('click', listClickManager);
+    $cancelPopup.addEventListener('click', closePopup);
+    $closeX.addEventListener('click', closePopup);
+    $okPopUp.addEventListener('click', acceptChangeHandler);
+    $popUpInput.addEventListener('keypress', acceptChangeHandlerOnEnter);//dodaje z popup na enter
 }
 
 function prepareInitialList() {
-    //wrzucenie poczatkowych elementów do listy
     initialList.forEach(todo => {
         addNewElementToList(todo, id);
         id++;
     });
-   
 }
 
 function addButtonClickHandler() {
-    //dodaje do lity wartośc z pola Input, jeżeli niepuste
     if ($myInput.value !== "") {
         addNewElementToList($myInput.value, id);
+        id++; 
+    }
+    $myInput.value = "";
+}
+
+function enterPushHandler() {
+    //dodaje z input po nacisnięciu enter
+    if ($myInput.value !== "" && event.keyCode === 13) {
+        addNewElementToList($myInput.value, id);
+        id++;
+        $myInput.value = "";   
     }
 }
 
@@ -45,23 +69,79 @@ function addNewElementToList(title, id) {
 
 function createElement(title , id) {
     const newElement = document.createElement('li');
-    newElement.innerHTML = '<div id="'+ id + '">' + title + 
-    '<span id="deleteTodo" class="delete"> Delete </span>' +
-    '<span id="editTodo" class="edit"> Edit </span>' +
-    '<span id="doneTodo" class="done"> Done </span>' +
+    newElement.setAttribute('id', id);
+    newElement.innerHTML = '<div>' + 
+    '<span id="titleToDo">' + title + '</span>' +
+    '<span id="deleteTodo" class="deleteBtn"> Delete </span>' +
+    '<span id="editTodo" class="editBtn"> Edit </span>' +
+    '<span id="statusTodo" class="doneBtn"> Done </span>' +
     '</div>';
     return newElement;
 }
 
 function listClickManager(event) {
-    // sprawdza co zostało klikniete (robocze)
-    if (event.target.className === 'done') {
-        console.log('done');
-    } else if (event.target.className === 'edit') {
-        console.log('edit');
-    } else if (event.target.className === 'delete') {
-        console.log('delete');
-    } else console.log(event.target.id);
+    let selectedId;
+    selectedId = event.target.parentElement.parentElement.id;
+    editedElement = event.target.parentElement.parentElement;
+    if (event.target.className === 'doneBtn') {
+        markElementAsDone(selectedId);
+    } else if (event.target.className === 'editBtn' && event.target.parentElement.className !== 'done') {
+        // nie edytuje jak done
+        openPopup(selectedId);
+    } else if (event.target.className === 'deleteBtn') {
+        removeListElement(selectedId);
+    } else if (event.target.className === 'doneBtn back') {
+        markElementAsNotDone(selectedId); 
+    }
+}
+
+function removeListElement(selectedId) {
+    document.getElementById(selectedId).remove();
+}
+
+function markElementAsDone(selectedId) {
+    //zmienia status na "done" i zmienia Btn na Back (powala na powrót do pierowtnego stanu)
+    changeStatusTodoClassesToggler(event.target);
+    event.target.innerText = "Back";
+}
+
+function markElementAsNotDone(selectedId) {
+    //task wraca jako niezrobiony
+    changeStatusTodoClassesToggler(event.target);
+    event.target.innerText = "Done";
+}
+
+function changeStatusTodoClassesToggler(toDoElement) {
+    toDoElement.parentElement.classList.toggle('done');
+    toDoElement.classList.toggle('back');
+}
+
+function openPopup(selectedId) {
+    popup.classList.add('show');
+    addDataToPopup(selectedId);
+}
+
+function closePopup(selectedId) {
+    popup.classList.remove('show'); 
+}
+
+function addDataToPopup(selectedId) {
+    let editedTask = document.getElementById(selectedId);
+    let editedText = editedTask.querySelector('span').innerText;
+    $popUpInput.value = editedText;
+}
+
+function acceptChangeHandler(id) {
+    editedElement.querySelector('span').innerText = $popUpInput.value;
+    closePopup();
+}
+
+function acceptChangeHandlerOnEnter(id) {
+    //edytuje po naciśnięciu enter
+    if (event.keyCode === 13) {
+        editedElement.querySelector('span').innerText = $popUpInput.value;
+        closePopup();
+    }
 }
 
 document.addEventListener('DOMContentLoaded', main);
